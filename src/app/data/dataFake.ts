2e9ba37d31afe7568442d62d48dffe12e301a1e9
VivianeOliveira <viviany-oliveira@outlook.com>
2023-10-15 20:50:20 -0300

    Foi feito modificações referentes ao conteúdo dos filmes e séries, acrescentei o logo no título, git hub, LinkedIn

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,34 @@
 export const dataFake = [
   {
-    "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    id: '1',
+    title: 'Guardiões da Galáxia 3',
+    description:
+      'Após a perda de Gamora, Peter Quill ainda está chateado e deve reunir os Guardiões da Galáxia em uma missão para defender o universo e proteger Rocket.',
+    photoCover:
+      'https://disneyplusbrasil.com.br/wp-content/uploads/2023/05/Guardioes-da-Galaxia-Vol3.jpg',
   },
   {
-    "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
-  }
-]
+    id: '2',
+    title: '2ª temporada de Loki',
+    description:
+      'Após ter roubado o Tesseract, um cubo cósmico poderoso, em Vingadores: Ultimato (2019), uma versão alternativa do vilão Loki é detida pela Autoridade de Variância Temporal (Time Variance Authority, no original). A organização existe para manter a ordem dentro do multiverso e evitar falhas na estrutura do tempo.',
+    photoCover:
+      'https://disneyplusbrasil.com.br/wp-content/uploads/2023/10/Loki-Tom-Hiddleston.jpg',
+  },
+  {
+    id: '3',
+    title: 'Invasão Secreta',
+    description:
+      'Após eventos recentes no Universo Marvel, o agente especial Nick Fury está desaparecido, mas poucos sabem seu verdadeiro paradeiro e identidade. A trama é ambientada nos “dias atuais”, quando Fury passa a ter conhecimento da invasão secreta de Skrulls que acontece na Terra.',
+    photoCover:
+      'https://disneyplusbrasil.com.br/wp-content/uploads/2023/07/Super-Skrull-e-Nick-Fury.jpg',
+  },
+  {
+    id: '4',
+    title: 'A Marvel Studios apresenta Echo',
+    description:
+      'Maya Lopez (Alaqua Cox) luta para se reconectar com suas raízes americanas nativas enquanto equilibra as aspirações ligadas a uma vida de crime como sucessora do legado brutal de Wilson Fisk (Vincent DOnofrio) também conhecido como Rei do Crime [Kingpin].',
+    photoCover:
+      'https://disneyplusbrasil.com.br/wp-content/uploads/2022/12/Echo-Alaqua-Cox.jpg',
+  },
+];
